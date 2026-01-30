021f09921dc60102315e5930e416641529d34744
Karat Sidhu <karatsingh@gmail.com>
2026-01-30 13:11:52 +0530

    continue on repo build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,51 @@
+use serde::Serialize;
+
+fn init_tracing() {
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info,tauri=info,app_lib=info"));
+
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_target(false)
+        .try_init();
+}
+
+#[derive(Serialize)]
+struct AppInfo {
+    name: String,
+    version: String,
+    identifier: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn ping() -> &'static str {
+    "pong"
+}
+
+#[tauri::command]
+fn app_info(app: tauri::AppHandle) -> AppInfo {
+    let package = app.package_info();
+    let config = app.config();
+    AppInfo {
+        name: package.name.clone(),
+        version: package.version.to_string(),
+        identifier: config.identifier.clone(),
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    init_tracing();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, ping, app_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
