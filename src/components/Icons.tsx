c7093f920f00297b3488fa9af4f1d5b70fa7282c
Karat Sidhu <karatsingh@gmail.com>
2026-02-02 11:49:40 +0530

    changed iicons from lucide react to hugeicons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,79 +1,234 @@
-export {
-  Plus,
-  Trash2,
-  FileText,
-  Layout,
-  Search,
-  Sparkles,
-  Bold,
-  Italic,
-  Strikethrough,
-  Code,
-  Quote,
-  List,
-  ListOrdered,
-  ListChecks,
-  Heading1,
-  Heading2,
-  Heading3,
-  Undo2,
-  Redo2,
-  Link2,
-  Minus,
-  Link,
-  Type,
-  StickyNote,
-  RefreshCw,
-  Frame,
-  Grid3X3,
-  AlignLeft,
-  AlignCenter,
-  AlignRight,
-  AlignStartVertical,
-  AlignCenterVertical,
-  AlignEndVertical,
-  AlignHorizontalSpaceAround,
-  AlignVerticalSpaceAround,
-  Paperclip,
-  RotateCcw,
-  Save,
-  ChevronRight,
-  ChevronDown,
-  FolderOpen,
-  FolderPlus,
-  Settings,
-  Zap,
-  X,
-  MessageSquare,
-  PanelRightOpen,
-  PanelRightClose,
-  PanelLeftOpen,
-  PanelLeftClose,
-  Bot,
-  Send,
-  Maximize2,
-  Minimize2,
-  File,
-  FileCode,
-  FileImage,
-  FileJson,
-  FileVideo,
-  FileAudio,
-  FileArchive,
-  FileSpreadsheet,
-  FileBox,
-  Image,
-  Film,
-  Music,
-  Archive,
-  Database,
-  Globe,
-  Cpu,
-  Palette,
-  Hash,
-  FileSymlink,
-  FileX,
-  FileCheck,
-  TriangleAlert,
-  CircleHelp,
-} from "lucide-react";
+import { HugeiconsIcon } from "@hugeicons/react";
+import * as Icons from "@hugeicons/core-free-icons";
+
+export const Plus = (props: any) => (
+  <HugeiconsIcon icon={Icons.Add} {...props} />
+);
+export const Trash2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Delete} {...props} />
+);
+export const FileText = (props: any) => (
+  <HugeiconsIcon icon={Icons.Document} {...props} />
+);
+export const Layout = (props: any) => (
+  <HugeiconsIcon icon={Icons.Layout} {...props} />
+);
+export const Search = (props: any) => (
+  <HugeiconsIcon icon={Icons.Search} {...props} />
+);
+export const Sparkles = (props: any) => (
+  <HugeiconsIcon icon={Icons.Sparkles} {...props} />
+);
+export const Bold = (props: any) => (
+  <HugeiconsIcon icon={Icons.Bold} {...props} />
+);
+export const Italic = (props: any) => (
+  <HugeiconsIcon icon={Icons.Italic} {...props} />
+);
+export const Strikethrough = (props: any) => (
+  <HugeiconsIcon icon={Icons.Strikethrough} {...props} />
+);
+export const Code = (props: any) => (
+  <HugeiconsIcon icon={Icons.Code} {...props} />
+);
+export const Quote = (props: any) => (
+  <HugeiconsIcon icon={Icons.Quote} {...props} />
+);
+export const List = (props: any) => (
+  <HugeiconsIcon icon={Icons.List} {...props} />
+);
+export const ListOrdered = (props: any) => (
+  <HugeiconsIcon icon={Icons.ListOrdered} {...props} />
+);
+export const ListChecks = (props: any) => (
+  <HugeiconsIcon icon={Icons.ListChecks} {...props} />
+);
+export const Heading1 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Heading1} {...props} />
+);
+export const Heading2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Heading2} {...props} />
+);
+export const Heading3 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Heading3} {...props} />
+);
+export const Undo2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Undo} {...props} />
+);
+export const Redo2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Redo} {...props} />
+);
+export const Link2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Link2} {...props} />
+);
+export const Minus = (props: any) => (
+  <HugeiconsIcon icon={Icons.Minus} {...props} />
+);
+export const Link = (props: any) => (
+  <HugeiconsIcon icon={Icons.Link} {...props} />
+);
+export const Type = (props: any) => (
+  <HugeiconsIcon icon={Icons.Text} {...props} />
+);
+export const StickyNote = (props: any) => (
+  <HugeiconsIcon icon={Icons.StickyNote} {...props} />
+);
+export const RefreshCw = (props: any) => (
+  <HugeiconsIcon icon={Icons.Refresh} {...props} />
+);
+export const Frame = (props: any) => (
+  <HugeiconsIcon icon={Icons.Frame} {...props} />
+);
+export const Grid3X3 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Grid} {...props} />
+);
+export const AlignLeft = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignLeft} {...props} />
+);
+export const AlignCenter = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignCenter} {...props} />
+);
+export const AlignRight = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignRight} {...props} />
+);
+export const AlignStartVertical = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignLeft} {...props} />
+);
+export const AlignCenterVertical = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignCenter} {...props} />
+);
+export const AlignEndVertical = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignRight} {...props} />
+);
+export const AlignHorizontalSpaceAround = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignLeft} {...props} />
+);
+export const AlignVerticalSpaceAround = (props: any) => (
+  <HugeiconsIcon icon={Icons.AlignCenter} {...props} />
+);
+export const Paperclip = (props: any) => (
+  <HugeiconsIcon icon={Icons.Paperclip} {...props} />
+);
+export const RotateCcw = (props: any) => (
+  <HugeiconsIcon icon={Icons.Refresh} {...props} />
+);
+export const Save = (props: any) => (
+  <HugeiconsIcon icon={Icons.Save} {...props} />
+);
+export const ChevronRight = (props: any) => (
+  <HugeiconsIcon icon={Icons.ArrowRight} {...props} />
+);
+export const ChevronDown = (props: any) => (
+  <HugeiconsIcon icon={Icons.ArrowDown} {...props} />
+);
+export const FolderOpen = (props: any) => (
+  <HugeiconsIcon icon={Icons.FolderOpen} {...props} />
+);
+export const FolderPlus = (props: any) => (
+  <HugeiconsIcon icon={Icons.FolderPlus} {...props} />
+);
+export const Settings = (props: any) => (
+  <HugeiconsIcon icon={Icons.Settings} {...props} />
+);
+export const Zap = (props: any) => (
+  <HugeiconsIcon icon={Icons.Zap} {...props} />
+);
+export const X = (props: any) => (
+  <HugeiconsIcon icon={Icons.Close} {...props} />
+);
+export const MessageSquare = (props: any) => (
+  <HugeiconsIcon icon={Icons.MessageSquare} {...props} />
+);
+export const PanelRightOpen = (props: any) => (
+  <HugeiconsIcon icon={Icons.SidebarRightIcon} {...props} />
+);
+export const PanelRightClose = (props: any) => (
+  <HugeiconsIcon icon={Icons.SidebarRightIcon} {...props} />
+);
+export const PanelLeftOpen = (props: any) => (
+  <HugeiconsIcon icon={Icons.SidebarLeftIcon} {...props} />
+);
+export const PanelLeftClose = (props: any) => (
+  <HugeiconsIcon icon={Icons.SidebarLeftIcon} {...props} />
+);
+export const Bot = (props: any) => (
+  <HugeiconsIcon icon={Icons.Bot} {...props} />
+);
+export const Send = (props: any) => (
+  <HugeiconsIcon icon={Icons.Send} {...props} />
+);
+export const Maximize2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Maximize} {...props} />
+);
+export const Minimize2 = (props: any) => (
+  <HugeiconsIcon icon={Icons.Minimize} {...props} />
+);
+export const File = (props: any) => (
+  <HugeiconsIcon icon={Icons.File} {...props} />
+);
+export const FileCode = (props: any) => (
+  <HugeiconsIcon icon={Icons.FileCode} {...props} />
+);
+export const FileImage = (props: any) => (
+  <HugeiconsIcon icon={Icons.FileImage} {...props} />
+);
+export const FileJson = (props: any) => (
+  <HugeiconsIcon icon={Icons.Files} {...props} />
+);
+export const FileVideo = (props: any) => (
+  <HugeiconsIcon icon={Icons.File} {...props} />
+);
+export const FileAudio = (props: any) => (
+  <HugeiconsIcon icon={Icons.File} {...props} />
+);
+export const FileArchive = (props: any) => (
+  <HugeiconsIcon icon={Icons.Archive} {...props} />
+);
+export const FileSpreadsheet = (props: any) => (
+  <HugeiconsIcon icon={Icons.File} {...props} />
+);
+export const FileBox = (props: any) => (
+  <HugeiconsIcon icon={Icons.FileX} {...props} />
+);
+export const Image = (props: any) => (
+  <HugeiconsIcon icon={Icons.Image} {...props} />
+);
+export const Film = (props: any) => (
+  <HugeiconsIcon icon={Icons.Film} {...props} />
+);
+export const Music = (props: any) => (
+  <HugeiconsIcon icon={Icons.Music} {...props} />
+);
+export const Archive = (props: any) => (
+  <HugeiconsIcon icon={Icons.Archive} {...props} />
+);
+export const Database = (props: any) => (
+  <HugeiconsIcon icon={Icons.Database} {...props} />
+);
+export const Globe = (props: any) => (
+  <HugeiconsIcon icon={Icons.Globe} {...props} />
+);
+export const Cpu = (props: any) => (
+  <HugeiconsIcon icon={Icons.Cpu} {...props} />
+);
+export const Palette = (props: any) => (
+  <HugeiconsIcon icon={Icons.Palette} {...props} />
+);
+export const Hash = (props: any) => (
+  <HugeiconsIcon icon={Icons.Hash} {...props} />
+);
+export const FileSymlink = (props: any) => (
+  <HugeiconsIcon icon={Icons.File} {...props} />
+);
+export const FileX = (props: any) => (
+  <HugeiconsIcon icon={Icons.FileX} {...props} />
+);
+export const FileCheck = (props: any) => (
+  <HugeiconsIcon icon={Icons.FileCheck} {...props} />
+);
+export const TriangleAlert = (props: any) => (
+  <HugeiconsIcon icon={Icons.Warning} {...props} />
+);
+export const CircleHelp = (props: any) => (
+  <HugeiconsIcon icon={Icons.Help} {...props} />
+);
