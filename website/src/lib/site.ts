0f98357e4e194cc5f6fc2e74ac540da034d8355e
Karat Sidhu <karatsingh@gmail.com>
2026-03-13 17:29:45 +0530

    Polish wikilinks graphics

--- website/src/lib/site.ts
+++ website/src/lib/site.ts
@@ -1,2 +1,2 @@
 export const downloadUrl =
-	"https://github.com/SidhuK/Glyph/releases/download/v0.1.43/Glyph_0.1.43_aarch64.dmg";
+	"https://github.com/SidhuK/Glyph/releases/download/v0.1.46/Glyph_0.1.46_aarch64.dmg";
