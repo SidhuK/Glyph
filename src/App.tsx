630f8d8f56b91696cc1417ebf223fdc259cf530f
SidhuK <karatsingh@gmail.com>
2026-09-20 09:17:41 +0530

    Reset agent chat across app boundaries

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import { LazyMotion, domAnimation } from "motion/react";
 import { AIConversationProvider } from "./components/ai/hooks/useRigChat";
 import { AppShell } from "./components/app/AppShell";
 import { LicenseGate } from "./components/licensing/LicenseGate";
-import { AppProviders } from "./contexts";
+import { AppProviders, useSpace } from "./contexts";
+
+function LicensedApp() {
+	const { spacePath } = useSpace();
+	return (
+		<AIConversationProvider key={spacePath ?? ""}>
+			<DragDropProvider>
+				<AppShell />
+			</DragDropProvider>
+		</AIConversationProvider>
+	);
+}
 
 function App() {
 	return (
 		<LazyMotion features={domAnimation}>
 			<AppProviders>
-				<AIConversationProvider>
-					<LicenseGate>
-						<DragDropProvider>
-							<AppShell />
-						</DragDropProvider>
-					</LicenseGate>
-				</AIConversationProvider>
+				<LicenseGate>
+					<LicensedApp />
+				</LicenseGate>
 			</AppProviders>
 		</LazyMotion>
 	);
